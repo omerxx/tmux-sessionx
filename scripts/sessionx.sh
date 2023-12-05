889224b8eaec1c4f4d9161f2a0199769a9582471
Omer Hamerman <omerxx@gmail.com>
2023-12-05 14:56:57 +0000

    better binding and order

--- scripts/sessionx.sh
+++ scripts/sessionx.sh
@@ -24,17 +24,24 @@ handle_output() {
     tmux switch-client -t "$target"
 }
 
+BIND_CTRL_D="ctrl-d:execute(tmux kill-session -t {})+reload(tmux list-sessions | sed -E 's/:.*$//' | grep -v $(tmux display-message -p '#S'))"
+BIND_CTRL_O="ctrl-o:print-query+execute(tmux new-session -d -s {})"
+BIND_CTRL_N="ctrl-n:reload(find ~/notes)"
+BIND_CTRL_X="ctrl-x:reload(find ~/dotfiles -mindepth 1 -maxdepth 1 -type d)"
+BIND_ENTER="enter:replace-query+print-query"
+
 RESULT=$(input | \
     fzf-tmux \
         -p "55%,55%" \
-	--prompt "(current:\"$CURRENT\")  " \
-        --header=' ^-d=delete ^-w=workspaces enter=switch/create' \
+	--prompt " " \
+        --header=' C-d=del C-n=notes C-x=config 󰿄=switch/create' \
 	--print-query \
-        --bind "ctrl-d:execute(tmux kill-session -t {})+reload(tmux list-sessions | sed -E 's/:.*$//' | grep -v $(tmux display-message -p '#S'))" \
-        --bind "ctrl-o:print-query+execute(tmux new-session -d -s {})" \
-        --bind "ctrl-w:reload(find ~/Documents '~/Documents/Obsidian\ Vault' -mindepth 1 -maxdepth 1 -type d)" \
-        --bind "ctrl-x:reload(find ~/dotfiles -mindepth 1 -maxdepth 1 -type d)" \
-        --bind=enter:replace-query+print-query \
+        --border-label "Current session: \"$CURRENT\" " \
+        --bind "$BIND_CTRL_D" \
+        --bind "$BIND_CTRL_O" \
+        --bind "$BIND_CTRL_N" \
+        --bind "$BIND_CTRL_X" \
+        --bind "$BIND_ENTER" \
         --exit-0 \
         --preview='tmux-prev {} | bat --theme TwoDark --style plain')
 
