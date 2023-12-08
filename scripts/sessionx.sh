abd760f89720a3a23a8b208e8a13e4f27672863f
Omer Hamerman <omerxx@gmail.com>
2023-12-08 09:21:22 +0000

    using local preview

--- scripts/sessionx.sh
+++ scripts/sessionx.sh
@@ -24,26 +24,24 @@ handle_output() {
 
 BIND_CTRL_D="ctrl-d:execute(tmux kill-session -t {})+reload(tmux list-sessions | sed -E 's/:.*$//' | grep -v $(tmux display-message -p '#S'))"
 BIND_CTRL_O="ctrl-o:print-query+execute(tmux new-session -d -s {})"
-BIND_CTRL_N="ctrl-n:reload(find ~/notes)"
 BIND_CTRL_X="ctrl-x:reload(find ~/dotfiles -mindepth 1 -maxdepth 1 -type d)"
 BIND_ENTER="enter:replace-query+print-query"
 BIND_CTRL_R='ctrl-r:execute(printf >&2 "New name: ";read name; tmux rename-session -t {} ${name};)+reload(tmux list-sessions | sed -E "s/:.*$//")'
 
 RESULT=$(input | \
     fzf-tmux \
-        -p "80%,70%" \
+        -p "75%,75%" \
 	--prompt " " \
-        --header=' C-d=del C-n=notes C-x=config C-r=rename 󰿄=go' \
+        --header='C-d=del C-x=config C-r=rename 󰿄=go' \
 	--print-query \
         --border-label "Current session: \"$CURRENT\" " \
         --bind "$BIND_CTRL_D" \
         --bind "$BIND_CTRL_O" \
-        --bind "$BIND_CTRL_N" \
         --bind "$BIND_CTRL_X" \
         --bind "$BIND_CTRL_R" \
         --bind "$BIND_ENTER" \
         --exit-0 \
-        --preview='tmux-prev {} | bat --theme TwoDark --style plain' \
-        --preview-window=",65%,,") 
+        --preview="${TMUX_PLUGIN_MANAGER_PATH}tmux-sessionx/scripts/preview.sh {} | bat --theme TwoDark --style plain" \
+        --preview-window=",55%,,") 
 
 handle_output "$RESULT"
