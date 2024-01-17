ae2f8a1e710fd730de34acf2a79580c5e0cf16c3
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-01-17 15:40:18 +0100

    removed leftover flake parts documentation

--- flake.nix
+++ flake.nix
@@ -7,12 +7,6 @@
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
-      ];
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {
         config,
@@ -23,11 +17,6 @@
         lib,
         ...
       }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
         packages.default = pkgs.tmuxPlugins.mkTmuxPlugin {
           pluginName = "sessionx";
           version = "1.0";
